7005b8be58bf4c3e52e1a3f116adb8e03cd70168
Caleb Saephan <calsyfur@gmail.com>
2025-07-08 22:26:05 -0700

    remove nuxt content - bettersql issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     compatibilityDate: "2025-05-15",
     devtools: { enabled: true },
 
-    modules: ["@nuxt/content", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
+    modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
     css: ["~/assets/css/main.css"],
     vite: {
         plugins: [tailwindcss()],
